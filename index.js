35ecf0fe690242aaf9b23decc97408bd5d5a3701
tunde Oretade <tundeiness@yahoo.co.uk>
2017-12-30 23:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(you){
-  return `Happy holidays, ${you}!`;
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidaysTo(holiday,name){
